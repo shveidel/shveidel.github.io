1685c52cb6875650920c11637ded96e2e5782750
Andrei Shveidel <asevos@gmail.com>
2026-09-09 15:44:26 +0300

    Deploy Dashboard hotfix

--- assets/chevron-up-B4cugfNq.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./index-CpYie9sq.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
---
+++ assets/chevron-up-CFsL8yLm.js
@@ -0,0 +1,1 @@
+import{R as e}from"./index-IbEn1tgc.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
