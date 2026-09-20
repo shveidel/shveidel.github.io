0e3f8ade59df73ce4031051045d215c600b7d323
Andrei Shveidel <asevos@gmail.com>
2026-09-21 01:18:25 +0300

    Deploy T-Bank and T-Invest UI improvements

---
+++ assets/chevron-up-SZHqfbx5.js
@@ -0,0 +1,1 @@
+import{it as e}from"./store-CvgmjTcR.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
--- assets/chevron-up-kOwrc7d-.js
+++
@@ -1,1 +0,0 @@
-import{$ as e}from"./store-BPu6Dooh.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
